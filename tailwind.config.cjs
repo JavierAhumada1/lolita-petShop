d639b284e7f17ca8656ae2eaac5780e796a0d284
JavierAhumada1 <javo2288@hotmail.com.ar>
2022-11-24 13:35:45 -0300

    Create component Header & styles

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,6 +6,18 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    colors:{
+      'orange':'#ED6436',
+      'green': '#65C178',
+      'light-grey' : '#F2F2F4',
+      'black': '#181818',
+      'dark-grey' : '#777777',
+      'white': '#fff',
+    },
+    fontFamily: {
+      'nunito': ['Nunito', 'sans-serif'],
+      'nunito-sans': ['Nunito Sans', 'sans-serif'],
+    },
   },
   plugins: [],
 }
