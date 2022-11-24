589014e440a0c998df254d6f66c819b92ec38d55
eugeniarossanigo <eugeniarossanigo@gmail.com>
2022-11-24 14:22:48 -0300

    welcome update with styles

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,23 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'welcome-img': "url('../public/lola-welcome.jpg')"
+      },
+      fontFamily: {
+        'nunito': ['Nunito', 'sans-serif'],
+        'nunito-sans': ['"Nunito Sans"', 'sans-serif']
+      }
+    },
+    colors: {
+      'orange':'#ED6436',
+      'green': '#65C178',
+      'light-grey': '#F2F2F4',
+      'black': '#181818',
+      'dark-grey': '#777777',
+      'white': '#fff',
+    }
   },
   plugins: [],
 }
