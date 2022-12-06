8a839b812b7682267683a174ae9b390a366eedf9
Matias98cs <matias98cs@gmail.com>
2022-12-06 15:59:57 -0300

    redux, store, userslice installed and created

---
+++ src/app/store.js
@@ -0,0 +1,7 @@
+import { configureStore } from "@reduxjs/toolkit";
+import userReducer from '../features/user/userSlice'
+
+const store = configureStore({
+    reducer: {user: userReducer}
+})
+export default store
