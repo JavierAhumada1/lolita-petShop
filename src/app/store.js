42a7b3e52cbbecb4c67322752e1f15cc65f12773
Matias98cs <matias98cs@gmail.com>
2022-12-15 21:48:34 -0300

    foodAPI redux created

--- src/app/store.js
+++ src/app/store.js
@@ -1,13 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "../features/user/userSlice";
 import { userAPI } from "../features/user/userAPI";
+import { foodAPI } from "../features/products/foodAPI";
 
 const store = configureStore({
   reducer: {
     [userAPI.reducerPath]: userAPI.reducer,
+    [foodAPI.reducerPath]: foodAPI.reducer,
     user: userReducer
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(userAPI.middleware),
+    getDefaultMiddleware().concat(userAPI.middleware, foodAPI.middleware),
 });
 export default store;
---
+++ src/features/products/foodAPI.js
@@ -0,0 +1,47 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const foodAPI = createApi({
+    reducerPath: 'foodAPI',
+    baseQuery: fetchBaseQuery({baseUrl: `${import.meta.env.VITE_BACKEND_URL}`}),
+    tagTypes: ["Food"],
+    endpoints: (builder) => ({
+        createProductFood: builder.mutation({
+            query: (foodProduct) => ({
+                url: `/food`,
+                method: 'POST',
+                body: foodProduct,
+                headers: {Authorization: 'Bearer ' + localStorage.getItem('token')}
+            }),
+            invalidatesTags: ['Food']
+        }),
+        updateProductFood: builder.mutation({
+            query: (newProductFood) => ({
+                url: `/food/${newProductFood._id}`,
+                method: 'PUT',
+                body: newProductFood,
+                headers: {Authorization: 'Bearer ' + localStorage.getItem('token')}
+            }),
+            invalidatesTags: ['Food']
+        }),
+        getProductsFood: builder.query({
+            query: () => ({
+                url: '/food',
+                method: 'GET',
+            }),
+            providesTags: ['Food']
+        }),
+        getProductFood: builder.query({
+            query: (id) => ({
+                url: `/food/${id}`,
+                method: 'GET',
+            })
+        })
+    })
+})
+
+export const {
+    useCreateProductFoodMutation,
+    useUpdateProductFoodMutation,
+    useGetProductsFoodQuery,
+    useGetProductFoodQuery
+} = foodAPI
